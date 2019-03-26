fd344cb64ed9e9c8b22915f07435c4584dddd9b3
Andy Hite <ahite@rabbet.com>
2019-03-26 17:29:38 -0500

    chore(setup): re-enable the homebrew and yadr install steps

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # Install homebrew
-# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Install applications
-# brew bundle
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install yadr
-# sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh`"
+sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh`"
 
 # Clone this repo into the home directory
 git clone https://github.com/andyhite/dotfiles.git ~/.dotfiles
+
+# Install applications
+cd ~/.dotfiles && brew bundle
 
 # Add custom zsh config
 ln -s "/Users/$(whoami)/.dotfiles/zsh.prompts" "/Users/$(whoami)/.zsh.prompts"
